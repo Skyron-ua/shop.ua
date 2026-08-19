1e4a90065b5a0f9ab125a02dcfd8c4be9810f490
Skyron-ua <dzhariko18@gmail.com>
2026-08-20 00:50:04 +0300

    Update config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ function trackPixelEvent(eventName, metaParams = {}, ttParams = {}) {
     window.ttq.track(ttEvent, ttParams);
   }
 }
-const $ = sel => document.querySelector(sel);
+const $ = window.$ || (sel => document.querySelector(sel));
 const $$ = sel => Array.from(document.querySelectorAll(sel));
 let cart = JSON.parse(localStorage.getItem('cart')) || [];
 // Модальне вікно "Додано в кошик"
